8dab84e0fe5cb1a4b09ef59947e8c67cbebd5604
dazzkaki <dazzkaki@konkuk.ac.kr>
2022-03-31 16:57:25 +0900

    Create split.py
    for train/validation/test split

---
+++ split.py
@@ -0,0 +1,3 @@
+import splitfolders
+
+splitfolders.ratio('C:/Users/Alien05/Downloads/TestProject_2022-03-31/masks/input', output="C:/Users/Alien05/Downloads/TestProject_2022-03-31/masks/output", seed=77, ratio=(0.7, 0.15, 0.15), group_prefix=2)
